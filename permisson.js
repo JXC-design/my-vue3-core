5604fa87e6d3514003a682f6661fde09212e09d7
Jixiong Chen <jxchenggmiao@gmail.com>
2025-01-14 17:24:24 +0800

    hasPermissionAndRole

--- permisson.js
+++ permisson.js
@@ -6,7 +6,12 @@ const hasRole = (user, role) => {
     return user.roles.includes(role)
 }
 
+const hasPermissionAndRole = (user, permission, role) => {
+    return hasPermission(user, permission) && hasRole(user, role)
+}
+
 export {
     hasPermission,
-    hasRole
+    hasRole,
+    hasPermissionAndRole
 }
