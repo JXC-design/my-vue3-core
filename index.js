a427f765afc2ea8130f8f8d2affc52904f324763
Jixiong Chen <jxchenggmiao@gmail.com>
2025-01-14 16:56:37 +0800

    add isPrime function

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ const reverseString = (str) => {
     return str.split('').reverse().join('')
 }
 
+const isPalindrome = (str) => {
+    return str === reverseString(str)
+}
+
+const isPrime = (num) => {
+    if (num <= 1) return false
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) return false
+    }
+    return true
+}
+
 export {
     addNum,
-    reverseString
+    reverseString,
+    isPalindrome,
+    isPrime
 }
